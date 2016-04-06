79d83f6f6690a314635d2211531515286e0ae158
Slevin Zhang <4ce111@gmail.com>
2016-04-06 19:27:47 -0400

    Add link to disabled users and user data factory

--- src/app/users/users.controller.js
+++ src/app/users/users.controller.js
@@ -6,10 +6,9 @@
     .controller('UsersController', UsersController);
 
   /** @ngInject */
-  function UsersController($log) {
+  function UsersController(UserService) {
     var vm = this;
 
-    vm.privileges = ['Admin', 'Member'];
     vm.roleClass = roleClass;
 
     activate();
@@ -24,51 +23,8 @@
         : 'md-accent md-hue-3';
     }
 
-    function getUsers() {
-      return [
-        {
-          name: 'David Parsons',
-          email: 'David.Parsons@GeorgianCollege.ca',
-          privilege: 'A',
-          active: true
-        },
-        {
-          name: 'Martin Pennock',
-          email: 'martin@gmail.com',
-          privilege: 'A',
-          active: true
-        },
-        {
-          name: 'Slevin',
-          email: 'slevin@gmail.com',
-          privilege: 'M',
-          active: false
-        },
-        {
-          name: 'Random Name',
-          email: 'random@random.org',
-          privilege: 'M',
-          active: false
-        },
-        {
-          name: 'John Shaw',
-          email: 'john@gmail.com',
-          privilege: 'A',
-          active: false
-        },
-        {
-          name: 'Rich Freeman',
-          email: 'rich@gmail.com',
-          privilege: 'M',
-          active: true
-        },
-        {
-          name: 'Scott McDonald',
-          email: 'scott@McDonald.com',
-          privilege: 'M',
-          active: true
-        }
-      ];
+    function getUsers(options) {
+      return UserService.getAll(options);
     }
 
   }
