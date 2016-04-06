cb21153edf1a80d6b6af0dd89b87586244ac9c60
Slevin Zhang <4ce111@gmail.com>
2016-04-06 17:32:06 -0400

    Finish basic users page layout

--- src/app/users/users.controller.js
+++ src/app/users/users.controller.js
@@ -9,12 +9,54 @@
   function UsersController($log) {
     var vm = this;
 
-    vm.random = 'hello';
+    vm.privileges = ['Admin', 'Member'];
+    vm.roleClass = roleClass;
 
     activate();
 
     function activate() {
-      $log.log('Users Controller loaded', vm.random);
+      vm.users = getUsers();
+    }
+
+    function roleClass(privilege) {
+      return privilege === 'A'
+        ? 'md-warn'
+        : 'md-accent md-hue-3';
+    }
+
+    function getUsers() {
+      return [
+        {
+          name: 'David Parsons',
+          email: 'David.Parsons@GeorgianCollege.ca',
+          privilege: 'A',
+          active: true
+        },
+        {
+          name: 'Martin Pennock',
+          email: 'martin@gmail.com',
+          privilege: 'A',
+          active: true
+        },
+        {
+          name: 'Slevin',
+          email: 'slevin@gmail.com',
+          privilege: 'M',
+          active: false
+        },
+        {
+          name: 'John Shaw',
+          email: 'john@gmail.com',
+          privilege: 'A',
+          active: false
+        },
+        {
+          name: 'Rich Freeman',
+          email: 'rich@gmail.com',
+          privilege: 'M',
+          active: true
+        }
+      ];
     }
 
   }
