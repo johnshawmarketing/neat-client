316fefc853590ec6e53c421a64f6083d5ddddaca
Slevin Zhang <4ce111@gmail.com>
2016-04-10 18:47:28 -0400

    Add detect current location

--- src/app/components/neatmap/map.directive.js
+++ src/app/components/neatmap/map.directive.js
@@ -36,11 +36,13 @@
     ) {
       var vm = this;
       var doc = $window.document;
+      var nav = $window.navigator;
       var markers = [];
       var records = [];
       var Gmap;
       var neatMap;
       var mapEl;
+      var geocoder;
 
       activate();
 
@@ -61,8 +63,9 @@
           zoom: 15
         });
 
-        getTypes();
+        geocoder = new Gmap.Geocoder;
 
+        getTypes();
         return getRecords().then(assignRecordsToMarkers);
       }
 
@@ -150,12 +153,14 @@
       } // showAdd
 
       function AddDialogController($scope, $mdDialog) {
+        var ad = $scope;
         $timeout(addPlaceSearch);
 
-        $scope.types = vm.types;
-        $scope.severity = 3;
-        $scope.cancel = cancel;
-        $scope.add = add;
+        ad.types = vm.types;
+        ad.severity = 3;
+        ad.cancel = cancel;
+        ad.add = add;
+        ad.locateCurrent = locateCurrent;
 
         function addPlaceSearch() {
           var input = byId('address');
@@ -169,9 +174,9 @@
             var places = searchBox.getPlaces();
             if (places.length === 0) return;
             var place = places[0];
-            $scope.address = place.name;
-            $scope.long = place.geometry.location.lng();
-            $scope.lat = place.geometry.location.lat();
+            ad.address = place.name;
+            ad.long = place.geometry.location.lng();
+            ad.lat = place.geometry.location.lat();
           });
         }
 
@@ -181,18 +186,44 @@
 
         function add() {
           MapData.createRecord({
-            long: $scope.long,
-            lat: $scope.lat,
-            address: $scope.address,
-            description: $scope.description,
-            severity: $scope.severity,
+            long: ad.long,
+            lat: ad.lat,
+            address: ad.address,
+            description: ad.description,
+            severity: ad.severity,
             user_id: $rootScope.user.id,
-            type_id: $scope.typeId
+            type_id: ad.typeId
           }).then(function(data) {
             var record = data.record;
-            magicSteveMarkerr($scope.lat, $scope.long, record, true);
+            magicSteveMarkerr(ad.lat, ad.long, record, true);
           });
           $mdDialog.hide();
+        }
+
+        function locateCurrent() {
+          ad.address = 'Locating...';
+          if (nav.geolocation) {
+            nav.geolocation.getCurrentPosition(function(pos) {
+              ad.lat = pos.coords.latitude;
+              ad.long = pos.coords.longitude;
+              geocoder.geocode({
+                location: { lat: ad.lat, lng: ad.long }
+              }, function(results, status) {
+                if (status === Gmap.GeocoderStatus.OK) {
+                  if (results[1]) {
+                    ad.address = results[1].formatted_address;
+                    ad.$apply();
+                  } else {
+                    $window.alert('No results found');
+                  }
+                } else {
+                  $window.alert('Geocoder failed due to: ' + status);
+                }
+              });
+            });
+          } else {
+            $log.error('Browser does not support goelocation');
+          }
         }
       } // dialogCtrl
 
